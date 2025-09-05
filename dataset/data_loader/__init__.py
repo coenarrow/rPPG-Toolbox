68d9bc79eecf7ad5f8f5500f802720e6f22cafa0
Coen Arrow <coen.arrow@gmail.com>
2025-09-05 08:53:04 +0800

    added Neckflix Loader to init

--- dataset/data_loader/__init__.py
+++ dataset/data_loader/__init__.py
@@ -9,3 +9,4 @@ import dataset.data_loader.BP4DPlusLoader
 import dataset.data_loader.BP4DPlusBigSmallLoader
 import dataset.data_loader.UBFCPHYSLoader
 import dataset.data_loader.PhysDriveLoader
+import dataset.data_loader.NeckflixLoader
